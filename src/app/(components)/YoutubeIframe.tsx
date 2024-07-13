5442bc3b8865269e2ecc4c8ec4bee33a883ff60b
jordanusmlementors <jordanusmlementors@gmail.com>
2024-07-14 02:31:12 +0300

    fix: error when video has no link

--- src/app/(components)/YoutubeIframe.tsx
+++ src/app/(components)/YoutubeIframe.tsx
@@ -1,15 +1,20 @@
 import videoLinkConverter from '../../utils/video/videoLinkConverter';
+import { Error } from '../../utils/video/videoLinkConverter';
 
 export default function YoutubeIframe({ link, className }: { link: string, className: string }) {
+  const videoLink = videoLinkConverter(link) === Error.NULL_VIDEO ? null : videoLinkConverter(link);
+
+  console.log("videoLink", videoLink);
+
   return (
     <div className={className}>
-      <iframe
+      {videoLink ? <iframe
         className="w-full"
         height="315"
-        src={videoLinkConverter(link)}
+        src={videoLink as string}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      ></iframe>
+      ></iframe> : <h1 className="text-3xl">This tutorial does not include videos</h1>}
     </div>
   );
 }
--- src/utils/video/videoLinkConverter.ts
+++ src/utils/video/videoLinkConverter.ts
@@ -1,9 +1,16 @@
 // required link format https://www.youtube.com/embed/[video_id]?si=TipNK-tholn1K98R
 
-export default function videoLinkConverter(link: string) {
-	if (link.startsWith('https://youtu.be')) {
-		return `https://www.youtube.com/embed/${link.split('/').pop()}?rel=0&showinfo=0&iv_load_policy=3`;
-	} else if (link.startsWith('https://www.youtube.com/watch')) {
-		return `https://www.youtube.com/embed/${link.substring(link.search(/=/gm) + 1)}?rel=0&showinfo=0&iv_load_policy=3`;
-	}
+export enum Error {
+	NULL_VIDEO
+}
+
+export default function videoLinkConverter(link: string): string | Error {
+	if (link)
+		if (link.startsWith('https://youtu.be')) {
+			return `https://www.youtube.com/embed/${link.split('/').pop()}?rel=0&showinfo=0&iv_load_policy=3`;
+		} else if (link.startsWith('https://www.youtube.com/watch')) {
+			return `https://www.youtube.com/embed/${link.substring(link.search(/=/gm) + 1)}?rel=0&showinfo=0&iv_load_policy=3`;
+		}
+
+	return Error.NULL_VIDEO;
 }
